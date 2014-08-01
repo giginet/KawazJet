43ffa9bbfef301b7c52b450547aee4b351ebfc93
giginet <giginet.net@gmail.com>
2014-08-01 10:15:45 +0900

    ステージ1を実装

---
+++ Resources/map/object.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Object" tilewidth="48" tileheight="48">
+ <image source="object.png" width="240" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="animation" value="hazard.png"/>
+   <property name="animationCount" value="2"/>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="category" value="8"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="category" value="16"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Resources/map/terrain.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Terrain" tilewidth="48" tileheight="48">
+ <image source="terrain.png" width="768" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="category" value="1"/>
+  </properties>
+ </tile>
+</tileset>
