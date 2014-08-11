8ce4daf5f30ab5a4d4ab255a393f532afee0bc63
giginet <giginet.net@gmail.com>
2014-08-11 16:47:07 +0900

    タイトル画面にボタンを設置した closes #2333 closes #2316

--- Classes/TitleScene.cpp
+++ Classes/TitleScene.cpp
@@ -37,23 +37,33 @@ bool TitleScene::init()
         return false;
     }
     
-    // 画面をタッチしたときにメイン画面へ遷移
-    auto listener = EventListenerTouchOneByOne::create();
-    listener->onTouchBegan = [](Touch* touch, Event* event) {
-        
-        auto main = MainScene::createSceneWithStage(0);
-        auto transition = TransitionCrossFade::create(0.5, main);
-        
-        Director::getInstance()->replaceScene(transition);
-        
-        return true;
-    };
-    this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
-    
     auto winSize = Director::getInstance()->getWinSize();
     auto background = Sprite::create("title.png");
     background->setPosition(Vec2(winSize.width / 2.0, winSize.height / 2.0));
     this->addChild(background);
+    
+    auto start = Sprite::create("start.png");
+    this->addChild(start);
+    start->setPosition(Vec2(winSize.width / 2, winSize.height / 4));
+    
+    // 画面をタッチしたときにメイン画面へ遷移
+    auto listener = EventListenerTouchOneByOne::create();
+    listener->onTouchBegan = [this, start](Touch* touch, Event* event) {
+        start->setTexture(Director::getInstance()->getTextureCache()->addImage("start_pressed.png"));
+        
+        this->getEventDispatcher()->removeAllEventListeners();
+        
+        this->runAction(Sequence::create(DelayTime::create(1.0),
+                                         CallFunc::create([] {
+            auto main = MainScene::createSceneWithStage(0);
+            auto transition = TransitionCrossFade::create(0.5, main);
+            
+            Director::getInstance()->replaceScene(transition);
+        }), NULL));
+        
+        return true;
+    };
+    this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
     
     return true;
 }
